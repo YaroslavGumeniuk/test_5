87814dab35d9b5daba7774a8ec19419f0b754c16
Yaroslav Cumeniuk <gumenyuk-ins@ukr.net>
2022-03-28 07:27:11 +0300

    feature/block_3: Hello controller added

--- cartridges/app_custom_storefront/cartridge/controllers/Hello.js
+++ cartridges/app_custom_storefront/cartridge/controllers/Hello.js
@@ -1,27 +1,10 @@
 'use strict';
-
-/**
- * @namespace Hello
- */
-
-var server = require('server');
-var cache = require('*/cartridge/scripts/middleware/cache');
-
-server.get('Start', cache.applyDefaultCache, function (req, res, next) {
-    var Site = require('dw/system/Site');
-    // var PageMgr = require('dw/experience/PageMgr');
-    // var pageMetaHelper = require('*/cartridge/scripts/helpers/pageMetaHelper');
-
-    // pageMetaHelper.setPageMetaTags(req.pageMetaData, Site.current);
-
-    // var page = PageMgr.getPage('homepage');
-
-    // if (page && page.isVisible()) {
-    //     res.page('homepage');
-    // } else {
-    //     res.render('home/homePage');
-    // }
+const server = require('server');
+server.get('Show', function (req, res, next) {
+    var template = 'hello';
+    res.render(template, {
+        message: 'Hello controller'
+    });
     next();
 });
-
 module.exports = server.exports();
