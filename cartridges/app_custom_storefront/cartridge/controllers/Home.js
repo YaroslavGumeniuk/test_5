2f60bbccb0d41928a69a0708090c3b75e7ebb5c9
Yaroslav Cumeniuk <gumenyuk-ins@ukr.net>
2022-03-28 09:01:07 +0300

    feature/block_3: Use Prepend, Append and Replace to Extend a Controller

--- cartridges/app_custom_storefront/cartridge/controllers/Home.js
+++ cartridges/app_custom_storefront/cartridge/controllers/Home.js
@@ -3,21 +3,40 @@
 const server = require('server');
 server.extend(module.superModule);
 
-server.prepend('Show', function (req, res, next) {
-    // const accountHelpers = require('*/cartridge/scripts/account/accountHelpers.js');
-    // res.setViewData({
-    //     randomString: accountHelpers.getRandomString('From controller ')
+var cache = require('*/cartridge/scripts/middleware/cache');
+
+server.prepend('Show', cache.applyDefaultCache, function (req, res, next) {
+    // res.render('homePage', {
+    //     param1: 'Site.current.name'
     // });
-    res.render('homePage', {
-        param1: 'Site.current.name'
+    // next();
+    var viewData = res.getViewData();
+    viewData.param1 = 'This is from prepend';
+    res.setViewData(viewData);
+    next();
+});
+
+server.append('Show', cache.applyDefaultCache, function (req, res, next) {
+    var viewData = res.getViewData();
+    // declare param1 as a variable that equals 'General company details.'
+    var appendParam = 'This is from append';
+    var queryParam = req.querystring.param ? req.querystring.param : 'no parameter was passed';
+    // Here grab whatever prepend added to viewData + the message here + the optional query string param
+    res.setViewData({
+        param1: viewData.param1 + ' AND ' + appendParam + ' AND querystring param = ' + queryParam
     });
     next();
 });
 
-// const server = require('server');
-// server.get('Show', function (req, res, next) {
-//     res.render('homePage', {param1: 'Site.current.name'});
-//     // res.render('helloWorld');
+// server.replace('Show', cache.applyDefaultCache, function (req, res, next) {
+//     var viewData = res.getViewData();
+//     // declare param1 as a variable that equals 'General company details.'
+//     var appendParam = 'This is from append';
+//     var replaceParam = req.querystring.param ? req.querystring.param : 'no parameter was passed';
+//     // Here grab whatever prepend added to viewData + the message here + the optional query string param
+//     res.setViewData({
+//         param1: viewData.param1 + ' AND ' + appendParam + ' AND ' + replaceParam
+//     });
 //     next();
 // });
 
