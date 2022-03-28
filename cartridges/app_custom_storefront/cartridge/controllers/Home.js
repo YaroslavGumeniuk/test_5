0456af4fae149c524b897342353a27957866ac7c
Yaroslav Cumeniuk <gumenyuk-ins@ukr.net>
2022-03-28 09:38:04 +0300

    feature/block_3: Extended the Cache Middleware

--- cartridges/app_custom_storefront/cartridge/controllers/Home.js
+++ cartridges/app_custom_storefront/cartridge/controllers/Home.js
@@ -16,14 +16,15 @@ server.prepend('Show', cache.applyDefaultCache, function (req, res, next) {
     next();
 });
 
-server.append('Show', cache.applyDefaultCache, function (req, res, next) {
+server.append('Show', cache.applyCustomCache, function (req, res, next) {
     var viewData = res.getViewData();
     // declare param1 as a variable that equals 'General company details.'
     var appendParam = 'This is from append';
     var queryParam = req.querystring.param ? req.querystring.param : 'no parameter was passed';
     // Here grab whatever prepend added to viewData + the message here + the optional query string param
     res.setViewData({
-        param1: viewData.param1 + ' AND ' + appendParam + ' AND querystring param = ' + queryParam
+        param1: viewData.param1 + ' AND ' + appendParam + ' AND querystring param = ' + queryParam,
+        param2: res.cachePeriod + ' ' + res.cachePeriodUnit
     });
     next();
 });
