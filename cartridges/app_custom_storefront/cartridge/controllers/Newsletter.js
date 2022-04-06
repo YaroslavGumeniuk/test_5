2f41837ec763045c824c693aea9debab79bbd830
Yaroslav Cumeniuk <gumenyuk-ins@ukr.net>
2022-04-06 17:09:00 +0300

    feature/block_7: Display Data Submitted From a Form

--- cartridges/app_custom_storefront/cartridge/controllers/Newsletter.js
+++ cartridges/app_custom_storefront/cartridge/controllers/Newsletter.js
@@ -18,4 +18,28 @@ server.get(
     }
 );
 
+server.post(
+    'Handler',
+    server.middleware.https,
+    function (req, res, next) {
+        var newsletterForm = server.forms.getForm('newsletter');
+        var continueUrl = dw.web.URLUtils.url('Newsletter-Show');
+        // Perform any server-side validation before this point, and invalidate form accordingly
+        if (newsletterForm.valid) {
+            // Send back a success status, and a redirect to another route
+            res.render('/newsletter/newslettersuccess', {
+                continueUrl: continueUrl,
+                newsletterForm: newsletterForm
+            });
+        } else if (!newsletterForm.valid) {
+            // Handle server-side validation errors here: this is just an example
+            res.render('/newsletter/newslettererror', {
+                errorMsg: dw.web.Resource.msg('error.crossfieldvalidation', 'newsletter', null),
+                continueUrl: continueUrl
+            });
+        }
+        next();
+    }
+);
+
 module.exports = server.exports();
