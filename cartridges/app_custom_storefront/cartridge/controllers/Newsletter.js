64a6cfc71b6ae44a06093ca6181e601754f4b384
Yaroslav Cumeniuk <gumenyuk-ins@ukr.net>
2022-04-20 09:31:21 +0300

    Middleware Chain Event Used

--- cartridges/app_custom_storefront/cartridge/controllers/Newsletter.js
+++ cartridges/app_custom_storefront/cartridge/controllers/Newsletter.js
@@ -38,7 +38,6 @@ server.post(
     server.middleware.https,
     csrfProtection.validateAjaxRequest,
     function (req, res, next) {
-        var Transaction = require('dw/system/Transaction');
         var newsletterForm = server.forms.getForm('newsletter');
         var continueUrl = dw.web.URLUtils.url('Newsletter-Show');
 
@@ -48,53 +47,57 @@ server.post(
             newsletterForm.email.value === newsletterForm.emailconfirm.value;
 
         if (newsletterForm.valid) {
-            try {
-                Transaction.wrap(function () {
-                    //Keep the same code you had inside the try block above
-                    var CustomObjectMgr = require('dw/object/CustomObjectMgr');
-                    var co = CustomObjectMgr.createCustomObject('NewsletterSubscription', newsletterForm.email.value);
-                    // var co = CustomObjectMgr.createCustomObject('subscription', newsletterForm.email.value);
-                    co.custom.firstName = newsletterForm.fname.value;
-                    co.custom.lastName = newsletterForm.lname.value;
 
-                    res.json({
-                        success: true,
-                        redirectUrl: URLUtils.url('Newsletter-Success').toString()
+            var Transaction = require('dw/system/Transaction');
+            this.on('route:BeforeComplete', function (req, res) {
+                try {
+                    Transaction.wrap(function () {
+                        //Keep the same code you had inside the try block above
+                        var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+                        var co = CustomObjectMgr.createCustomObject('NewsletterSubscription', newsletterForm.email.value);
+                        // var co = CustomObjectMgr.createCustomObject('subscription', newsletterForm.email.value);
+                        co.custom.firstName = newsletterForm.fname.value;
+                        co.custom.lastName = newsletterForm.lname.value;
+
+                        res.json({
+                            success: true,
+                            redirectUrl: URLUtils.url('Newsletter-Success').toString()
+                        });
                     });
-                });
-            } catch (e) {
-                var err = e;
-                var Resource = require('dw/web/Resource');
-                if (err.javaName === "MetaDataException") {
-                    /* Duplicate primary key on CO: send back message to client-side, but don't log error.
-                        This is possible if the user tries to subscribe with the same email multiple times */
-                    res.json({
-                        success: false,
-                        error: [Resource.msg('error.subscriptionexists', 'newsletter', null)]
-                    });
-                } else {
-                    /* Missing CO definition: Log error with message for site admin, set the response to error, and send error page URL to client-side */
-                    var Logger = require('dw/system/Logger');
-                    Logger.getLogger("newsletter_subscription").error(Resource.msg('error.customobjectmissing', 'newsletter', null));
-                    // Show general error page: there is nothing else to do
+                } catch (e) {
+                    var err = e;
+                    var Resource = require('dw/web/Resource');
+                    if (err.javaName === "MetaDataException") {
+                        /* Duplicate primary key on CO: send back message to client-side, but don't log error.
+                            This is possible if the user tries to subscribe with the same email multiple times */
+                        res.json({
+                            success: false,
+                            error: [Resource.msg('error.subscriptionexists', 'newsletter', null)]
+                        });
+                    } else {
+                        /* Missing CO definition: Log error with message for site admin, set the response to error, and send error page URL to client-side */
+                        var Logger = require('dw/system/Logger');
+                        Logger.getLogger("newsletter_subscription").error(Resource.msg('error.customobjectmissing', 'newsletter', null));
+                        // Show general error page: there is nothing else to do
+                        // res.setStatusCode(500);
+                        // res.json({
+                        //     error: true,
+                        //     redirectUrl: URLUtils.url('Error-Start').toString()
+                        // });
+
+                        // Handle server-side validation errors here: this is just an example
+                        res.json({
+                            success: false,
+                            error: [Resource.msg('error.crossfieldvalidation', 'newsletter', null)]
+                        });
+                    }
                     // res.setStatusCode(500);
                     // res.json({
                     //     error: true,
                     //     redirectUrl: URLUtils.url('Error-Start').toString()
                     // });
-
-                    // Handle server-side validation errors here: this is just an example
-                    res.json({
-                        success: false,
-                        error: [Resource.msg('error.crossfieldvalidation', 'newsletter', null)]
-                    });
-                }
-                // res.setStatusCode(500);
-                // res.json({
-                //     error: true,
-                //     redirectUrl: URLUtils.url('Error-Start').toString()
-                // });
-            }
+                } //end try-catch
+            }); //end BeforeComplete
             ////////////////////////////////////////////////////////////////////////////////////////////////
             // Send back a success status, and a redirect to another route
             // res.render('/newsletter/newslettersuccess', {
